568f15f9e30522eb315ea42f7dee27f828a1e415
EcossB <erwingcoss21@hotmail.com>
2023-12-21 19:26:58 -0400

    Creating custom query

--- src/main/java/com/foroAlura/ForoAluraApi/domain/foro/TopicsRepository.java
+++ src/main/java/com/foroAlura/ForoAluraApi/domain/foro/TopicsRepository.java
@@ -1,6 +1,10 @@
 package com.foroAlura.ForoAluraApi.domain.foro;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicsRepository extends JpaRepository<Topics, Long> {
+
+     Page<Topics> findByActiveTrue(Pageable pageable);
 }
