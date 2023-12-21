64422aaf19c12b5270cc3dc12fd44f3747446e4f
EcossB <erwingcoss21@hotmail.com>
2023-12-21 18:52:40 -0400

    Topics Service

---
+++ src/main/java/com/foroAlura/ForoAluraApi/domain/foro/TopicsService.java
@@ -0,0 +1,35 @@
+package com.foroAlura.ForoAluraApi.domain.foro;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class TopicsService {
+
+    /*
+    implementing Solid Principles.
+     */
+
+    @Autowired
+    private TopicsRepository topicsRepository;
+
+
+    public ResponseEntity<?> saveTopic(TopicsData data, UriComponentsBuilder builder){
+
+        Topics topic = topicsRepository.save(new Topics(data));
+
+        TopicsResponseDto topicsResponseDto = new TopicsResponseDto(topic.getId(), topic.getTitle(), topic.getMessage(),
+                topic.getCreation_date(), topic.isTopic_status() ,topic.getAuthor());
+
+        URI url = builder.path("/topic/{id}").buildAndExpand(topic.getId()).toUri();
+        return ResponseEntity.created(url).body(topicsResponseDto);
+
+    }
+
+
+}
