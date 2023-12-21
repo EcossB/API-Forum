88263a1637246dbeb6273e0d87090354175c214c
EcossB <erwingcoss21@hotmail.com>
2023-12-21 19:27:35 -0400

    Creating logic for getmethod.

--- src/main/java/com/foroAlura/ForoAluraApi/domain/foro/TopicsService.java
+++ src/main/java/com/foroAlura/ForoAluraApi/domain/foro/TopicsService.java
@@ -2,6 +2,7 @@ package com.foroAlura.ForoAluraApi.domain.foro;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -24,11 +25,17 @@ public class TopicsService {
         Topics topic = topicsRepository.save(new Topics(data));
 
         TopicsResponseDto topicsResponseDto = new TopicsResponseDto(topic.getId(), topic.getTitle(), topic.getMessage(),
-                topic.getCreation_date(), topic.isTopic_status() ,topic.getAuthor());
+                topic.getCreation_date(), topic.isActive() ,topic.getAuthor());
 
         URI url = builder.path("/topic/{id}").buildAndExpand(topic.getId()).toUri();
         return ResponseEntity.created(url).body(topicsResponseDto);
 
+    }
+
+
+    public ResponseEntity<?> getTopics(Pageable pageable){
+
+        return ResponseEntity.ok(topicsRepository.findByActiveTrue(pageable).map(TopicsResponseDto::new));
     }
 
 
