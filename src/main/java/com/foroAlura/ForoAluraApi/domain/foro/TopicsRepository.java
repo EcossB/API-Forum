3f87c3069a1cb62a3f17c01179731cec30cd4794
EcossB <erwingcoss21@hotmail.com>
2023-12-21 18:53:08 -0400

    Repository for data managment

---
+++ src/main/java/com/foroAlura/ForoAluraApi/domain/foro/TopicsRepository.java
@@ -0,0 +1,6 @@
+package com.foroAlura.ForoAluraApi.domain.foro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicsRepository extends JpaRepository<Topics, Long> {
+}
